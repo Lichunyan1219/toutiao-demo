659887127c0cc05e64570c1daf2ca509a76dc18b
三叶 <rzh466470@outlook.com>
2022-07-13 12:21:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="title">头条</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.title {
+  font-size: 30px;
+  width: 100px;
+  height: 100px;
+  background-color: pink;
+}
+</style>
